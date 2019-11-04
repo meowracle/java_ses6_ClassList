b5e1490f2f7b94e900db79a0f1454aa16cefc2e1
meowracle <aprilerd34@gmail.com>
2019-11-04 14:41:30 +0700

    add ex

---
+++ src/MyList.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class MyList<E> {
+    public int size = 0;
+    public static final int DEFAULT_CAPACITY = 10;
+    public Object[] elements;
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = e;
+    }
+
+    public E get(int i) {
+        if (i > size || i < 0) {
+            throw new IndexOutOfBoundsException("Index " + i + ", size " + i);
+        }
+        return (E) elements[i];
+    }
+
+
+}
